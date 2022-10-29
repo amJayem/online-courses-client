32c859ee9d6e4eb13774b89e8063cd0dba44a93b
amJayem <csejayem19@gmail.com>
2022-10-29 15:46:47 +0600

    spinner added

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -4,7 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 const Cart = () => {
     const course = useLoaderData();
     return (
-        <div>
+        <div className='container'>
             <h1>Welcome to Checkout</h1> <hr />
             <h3>Course Name: {course.courseName}</h3>
             <h4>Course ID: {course.id}</h4>
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import { useContext } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { AuthContext } from '../contexts/AuthProvider';
+import React from "react";
+import { useContext } from "react";
+import { Dna } from "react-loader-spinner";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthProvider";
 
-const PrivateRoute = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
-    const location = useLocation();
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
 
-    // console.log(user);
+  // console.log(user);
 
-    if(loading){
-        return <>Loading..</>
-    }
+  if (loading) {
+    return (
+      <div className="mx-auto my-0">
+        <Dna
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+        />
+      </div>
+    );
+  }
 
-    if(!user){
-        return <Navigate to='/login' state={{from:location}} replace></Navigate>
-    }
-    return children;
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+  return children;
 };
 
 export default PrivateRoute;
